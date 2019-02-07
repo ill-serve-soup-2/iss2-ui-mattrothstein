ec3f37183a3fe055a2be91b37d092bce5402fe1b
mattr21 <matt.rothstein21@gmail.com>
2019-02-06 23:58:41 -0800

    added typewriter effect

--- js/header.js
+++ js/header.js
@@ -1,2 +1,17 @@
 
 
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('manual inventory?')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('wasting money?')
+    .pauseFor(2500)
+    .deleteChars(6)
+    .typeString('time?')
+    .pauseFor(2500)
+    .start();
